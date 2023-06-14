c662f72fee3cd7a8d63db93976606a48171ff58b
MiharoYi <2318609881@qq.com>
2023-06-14 19:58:29 +0800

    Update index.vue

--- src/components/TheCarousel/index.vue
+++ src/components/TheCarousel/index.vue
@@ -20,28 +20,28 @@
 
 <script>
 export default {
-  name: "TheCarousel",
-  data() {
+name: "TheCarousel",
+data() {
     return {};
-  },
-  methods: {},
+},
+methods: {},
 };
 </script>
 
 <style>
 .el-carousel__item h3 {
-  color: #475669;
-  font-size: 14px;
-  opacity: 0.75;
-  line-height: 150px;
-  margin: 0;
+color: #475669;
+font-size: 14px;
+opacity: 0.75;
+line-height: 150px;
+margin: 0;
 }
 
 .el-carousel__item:nth-child(2n) {
-  background-color: #99a9bf;
+background-color: #99a9bf;
 }
 
 .el-carousel__item:nth-child(2n + 1) {
-  background-color: #d3dce6;
+background-color: #d3dce6;
 }
 </style>

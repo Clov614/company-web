d2c37e9e0e348ffa9b50193691e6c06368b9149c
MiharoYi <2318609881@qq.com>
2023-06-15 10:58:34 +0800

    Create UpDataAnnounce.vue

---
+++ src/components/UpDataAnnounce.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="container">
+    <h1 class="page-title">修改公告</h1>
+    <el-form
+      ref="announcementForm"
+      :model="form"
+      label-width="120px"
+      class="form-container"
+    >
+      <el-form-item label="标题">
+        <el-input v-model="form.title" class="input-large"></el-input>
+      </el-form-item>
+      <el-form-item label="公告内容">
+        <el-input
+          type="textarea"
+          v-model="form.content"
+          class="textarea-large"
+        ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button
+          type="primary"
+          @click="updateAnnouncement"
+          class="submit-button"
+          >更新公告</el-button
+        >
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        title: "",
+        content: "",
+      },
+    };
+  },
+  created() {
+    // 在页面加载时，可以从后端获取公告信息，并将其填充到表单中
+    this.fetchAnnouncement();
+  },
+  methods: {
+    fetchAnnouncement() {
+      // 从后端获取公告信息的逻辑
+      // 假设获取到的公告信息为announcementData
+      const announcementData = {
+        title: "",
+        content: "",
+      };
+      // 将公告信息填充到表单中
+      this.form.title = announcementData.title;
+      this.form.content = announcementData.content;
+    },
+    updateAnnouncement() {
+      // 执行更新公告的逻辑，可以将 this.form 提交到后端或进行其他操作
+      console.log("更新公告", this.form);
+    },
+  },
+};
+</script>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.page-title {
+  font-size: 24px;
+  margin-bottom: 20px;
+  text-align: center;
+}
+
+.form-container {
+  width: 500px;
+  padding: 40px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.el-form-item__label {
+  font-weight: bold;
+}
+
+.input-large .el-input__inner {
+  height: 40px;
+  font-size: 16px;
+  line-height: 40px;
+}
+
+.textarea-large .el-input__inner {
+  height: 40px;
+  font-size: 16px;
+  line-height: 40px;
+}
+
+.submit-button {
+  width: 100%;
+  margin-top: 20px;
+}
+</style>

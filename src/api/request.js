f0b812733e02ed7668336ac46ac601590a9d6e0e
Clov614 <541982545@qq.com>
2023-06-16 09:12:55 +0800

    Updata: 添加修改配置，解决axios跨域问题

--- src/api/request.js
+++ src/api/request.js
@@ -2,7 +2,12 @@
 import axios from 'axios'
 // 封装 baseURL
 const request = axios.create({
-    baseURL:'http://localhost:9000' // 后端地址(使用springMVC开发REST API)
+    baseURL:'/api', // 后端地址(使用springMVC开发REST API)
+    headers: {
+        post: {
+            "Content-Type": "application/json",
+        }
+    }
 })
 // 向外暴露 request
 export default request;
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    open: true, //是否启动打开浏览器
+    host: "0.0.0.0",//主机，0.0.0.0支持局域网地址，可以用真机测试
+    port: 8080, //端口
+    https: false,//是否启动https
+    //配置跨域代理http,https
+    proxy: {
+      "/api": {
+        target: "http://localhost:9000",
+        changeOrigin: true,
+        // pathRewrite: {
+        //   '^/api': ''
+        // }
+      }
+    }
+  },
 })

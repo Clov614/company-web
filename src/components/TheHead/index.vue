e0292eb5188311cbd74a005cb786839569dacdab
Clov614 <541982545@qq.com>
2023-06-17 16:38:28 +0800

    Update: nav跳转改为编程式跳转

--- src/components/TheHead/index.vue
+++ src/components/TheHead/index.vue
@@ -11,8 +11,8 @@
     <div class="line"></div>
     <el-menu :default-active="activeIndex2" class="el-menu-demo" mode="horizontal" @select="handleSelect"
       background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
-      <el-menu-item index="1"><router-link to='/home'>首页</router-link></el-menu-item>
-      <el-menu-item index="2"><router-link to='/news'>新闻</router-link></el-menu-item>
+      <el-menu-item index="1" @click="Go2Home">首页</el-menu-item>
+      <el-menu-item index="2" @click="Go2News">新闻</el-menu-item>
       <el-menu-item index="3"><a href="/home">重点关注</a></el-menu-item>
       <el-menu-item index="5" style="float: right;" v-show="!triggerSign"><router-link
           to='/login'>登录</router-link></el-menu-item>
@@ -44,6 +44,13 @@ export default {
   methods: {
     handleSelect(key, keyPath) {
       console.log(key, keyPath);
+    },
+    // 编程式跳转
+    Go2Home() {
+      this.$router.push("/home").catch(err => {err});
+    },
+    Go2News() {
+      this.$router.push("/news").catch(err => {err});
     },
     // init nick_name
     initNick() {
@@ -135,4 +142,8 @@ export default {
   left: 0;
   right: 0;
 }
+.el-menu-item * {
+  text-decoration: none;
+}
+
 </style>
